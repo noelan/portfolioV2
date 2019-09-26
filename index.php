de46ccc97bbc6ac2bbebc3d68ec9b18887f6ceb9
noel an <asiapro69@hotmail.fr>
2019-09-26 19:15:43 +0200

    some sm responsive

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	    	<h1 class="aPropos text-center myWhite">Compétences</h1>
 	    </div>
 	    	<div class="row">
-	    	<div class="aboutMeDiv col-lg-4 offset-lg-1">
+	    	<div class="aboutMeDiv col-lg-4 col-sm-12 offset-lg-1">
 				<p class="text-justify workSans presentation">
 					Bonjour je me présente Noël AN. Après avoir travailler principalement dans la restauration et la logistique, j'ai décider de me reconvertir dans un domaine qui m'a toujours passioné, le devéloppement Web.
 				</p>
@@ -26,7 +26,7 @@
 					De nature curieux et déterminé je fais en sorte d'approfondir mes connaissances quotidiennement et je quitte diffilement un projet sur un message d'erreur.
 				</p>
 			</div>	
-	        <div class="col-5 offset-lg-1">       	
+	        <div class="col-lg-5 offset-lg-1 col-sm-12">       	
 	        	<h3 class="progress-title">Symfony</h3>
 	            <div class="progress">
 	                <div class="progress-bar" style="width:75%; background:#0275d8">
@@ -57,48 +57,60 @@
 	                    <span class="progress-icon fa fa-rocket"></span>
 	                </div>
 	            </div>
+	            <h3 class="progress-title">Git</h3>
+	            <div class="progress">
+	                <div class="progress-bar" style="width:75%; background:#DB6363;">
+	                    <span class="progress-icon fa fa-rocket" style="border-color: #DB6363;color:#DB6363;"></span>
+	                </div>
+	            </div>
 	        </div>   
 	    </div>
 	</div>
 	<div class="myProject" id="project">
-		<h1 class="workSans myWhite text-center">Expériences et projets</h1>
+		<h1 class="workSans myWhite text-center font-weight-bold">Expériences et projets</h1><br>
 		<div class="container">
 			<div class="row justify-content-center">
-				<div class="col-8">
+				<div class="col-lg-8 col-sm-12">
 					<h3 class="aPropos text-center">Funny Shop</h3>
 					<p>Site de E commerce 'bac à sable' créer pour parfaire mes compétences et principalement avec le framework Symfony.</p>
 					<p>Mise en place de mail automatique lors de l'inscription, interface d'administration (CRUD), panier d'achat et en constante évolution !</p>
 					<a href="http://noel-an-fr.mon.world/funnyshop/public/index.php"><p class="text-center">Celui-ci est en ligne !</p></a>
+				<div class="text-center">
 					<p class="btn btn-primary">Symfony</p>
 					<p class="btn btn-success">Twig</p>
 					<p class="btn btn-info">Doctrine</p>
+				</div>	
 				</div>
-				<div class="col-4">
+				<div class="col-lg-4 col-sm-12">
 					<img class="myImg" src="assets/picture/funnyshop.png">
 				</div>
 				</div><hr>
 			<div class="row justify-content-center">
-				<div class="col-8">
+				<div class="col-lg-8 col-sm-12">
 					<h3 class="aPropos text-center">Event Team</h3>
 					<p>Site réalisé en cours de formation permettant de gerer le planning des utilisateur 'Google Agenda like'.</p>
 					<p>Mise en place du Modèle Vue Controlleur, travail en équipe (4 personnes)en méthode agile.</p>
+				<div class="text-center">	
 					<p class="btn btn-primary">PHP</p>
 					<p class="btn btn-success">Scrum</p>
 					<p class="btn btn-secondary">Git</p>
 					<p class="btn btn-warning">Js Vanilla</p>
 				</div>
-				<div class="col-4">
+				</div>
+				<div class="col-lg-4 col-sm-12">
 					<img class="myImg" src="https://images.pexels.com/photos/3299/postit-scrabble-to-do.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
 				</div>
 			</div><hr>
 			<div class="row justify-content-center">
-				<div class="col-8">
+				<div class="col-lg-8 col-sm-12">
 					<h3 class="aPropos text-center">Site de solfège débutant</h3>
 					<p>Site réalisé en parallèle de la formation permettant d'apprendre le solfège à travers des minis jeux.</p>
 					<p>Ce projet a été réalisé afin découvrir et approfondir le Javascript Vanilla</p>
+				<div class="text-center">
 					<p class="btn btn-warning">Javascript Vanilla</p>
 				</div>
-				<div class="col-4">
+				</div>
+				<div class="col-lg-4 col-sm-12">
 					<img class="myImg" src="https://images.pexels.com/photos/1210530/pexels-photo-1210530.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
 				</div>
 			</div>
@@ -107,9 +119,15 @@
 	<div class="contactMe" id="contactme">
 		<h3 class="courgette text-center" style="color:black;">Me contacter</h3>
 		<div class="row justify-content-center">
-			<p class="footerP"><i class="far fa-envelope"></i> Gmail : noel.an.lyon@gmail.com</p>
-			<p class="footerP"><i class="fas fa-phone-square"></i> Tel : 06 83 07 54 48</p>
-			<a href="https://www.linkedin.com/in/noel-an-704482186/"><p class="footerP"><i class="fas fa-phone-square"></i> Linkedin </p></a>
+			<div class="text-center col-lg-2 col-sm-12">
+				<p class="footerP"><i class="far fa-envelope"></i> Gmail : noel.an.lyon@gmail.com</p>
+			</div>
+			<div class="text-center col-lg-2 col-sm-12">
+				<p class="footerP"><i class="fas fa-phone-square"></i> Tel : 06 83 07 54 48</p>
+			</div>
+			<div class="text-center col-lg-2 col-sm-12">
+				<a href="https://www.linkedin.com/in/noel-an-704482186/"><p class="footerP"><i class="fas fa-phone-square"></i> Linkedin </p></a>
+			</div>
 		</div>
 	</div>
 	</div>	
